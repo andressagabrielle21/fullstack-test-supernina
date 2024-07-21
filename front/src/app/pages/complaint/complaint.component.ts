b201b12a8cc2ca456b0bc9193b3cb48830b5ce0d
Andressa <andressagss21@gmail.com>
2024-07-21 20:50:02 -0300

    initial frontend complaint page

--- front/src/app/pages/complaint/complaint.component.ts
+++ front/src/app/pages/complaint/complaint.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'complaint',
   standalone: true,
   templateUrl: './complaint.component.html',
+  imports: [ButtonComponent, RouterLink]
 })
 export class ComplaintComponent {
   @Input() complaintId = '';
